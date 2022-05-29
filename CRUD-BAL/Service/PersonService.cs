16be907f223aed3feb80007352452e71a39b6f54
Glenn Leonard Gaerlan <no_fear1972@yahoo.com>
2022-05-29 21:13:00 +0800

    initial commit

---
+++ CRUD-BAL/Service/PersonService.cs
@@ -0,0 +1,78 @@
+﻿using CRUD_DAL.Interface;
+using CRUD_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_BAL.Service
+{
+    public class PersonService
+    {
+        private readonly IRepository<Person> _person;
+
+        public PersonService(IRepository<Person> perosn)
+        {
+            _person = perosn;
+        }
+        //Get Person Details By Person Id
+        public IEnumerable<Person> GetPersonByUserId(string UserId)
+        {
+            return _person.GetAll().Where(x => x.LastName == UserId).ToList();
+        }
+        //GET All Perso Details 
+        public IEnumerable<Person> GetAllPersons()
+        {
+            try
+            {
+                return _person.GetAll().ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        //Add Person
+        public async Task<Person> AddPerson(Person Person)
+        {
+            return await _person.Create(Person);
+        }
+        //Delete Person 
+        public bool DeletePerson(string LastName)
+        {
+
+            try
+            {
+                var DataList = _person.GetAll().Where(x => x.LastName == LastName).ToList();
+                foreach (var item in DataList)
+                {
+                    _person.Delete(item);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+
+        }
+        //Update Person Details
+        public bool UpdatePerson(Person person)
+        {
+            try
+            {
+                var DataList = _person.GetAll().Where(x => x.IsDeleted != true).ToList();
+                foreach (var item in DataList)
+                {
+                    _person.Update(item);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ CRUDDAL/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CRUD_DAL.Models
+{
+    [Table("Persons", Schema = "dbo")]
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "CreatedOn")]
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        [Required]
+        [Display(Name = "IsDeleted")]
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
